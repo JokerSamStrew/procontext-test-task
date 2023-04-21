87853944507d0649176b617130fa31b3117533a1
Simon <s.labzoff@yandex.ru>
2023-04-21 09:13:33 +0300

    -cursVal conversion

--- src/main.go
+++ src/main.go
@@ -3,18 +3,34 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 	"time"
 )
 
-func main() {
-	currentDate := time.Now().Local().AddDate(0, 0, -90)
-
-	valutes, err := RetrieveValCurs(currentDate)
+func cursValToFloat(value string) float64 {
+	curs, err := strconv.ParseFloat(strings.Replace(value, ",", ".", -1), 32)
 	if err != nil {
-		log.Fatalf("failed retrieve val curs: %v", err)
+		log.Fatalf("failed parse curs val: %v", err)
 	}
 
-	for _, valute := range valutes {
-		fmt.Println(valute)
+	return curs
+}
+
+func main() {
+	const (
+		DateRange = 90
+	)
+
+	for i := 0; i < DateRange; i++ {
+		currentDate := time.Now().Local().AddDate(0, 0, -i)
+		valutes, err := RetrieveValCurs(currentDate)
+		if err != nil {
+			log.Fatalf("failed retrieve val curs: %v", err)
+		}
+
+		for _, valute := range valutes {
+			fmt.Println(cursValToFloat(valute.Value))
+		}
 	}
 }
